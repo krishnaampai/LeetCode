d44e5b5316c3f7923413ac7c462070f07fbc38c2
krishnaampai <147622221+krishnaampai@users.noreply.github.com>
2024-12-16 18:14:10 +0530

    [53] [Time Beats: 100.00%] [Memory Beats: 5.89%] - LeetPush

---
+++ 53-Maximum-Subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int current_max = nums[0];
+        int global_max = nums[0];
+        for (int i =1;i<nums.size(); i++){
+            current_max = max(nums[i], current_max + nums[i]);
+            if (current_max > global_max){
+                global_max = current_max ;
+            }
+        }
+        return global_max;
+        
+    }
+};
