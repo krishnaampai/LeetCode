0674b600773dc5b8ca91030ae4a112ab7bcb3e0d
krishnaampai <147622221+krishnaampai@users.noreply.github.com>
2024-12-24 14:26:09 +0530

    [2011] [Time Beats: 100.00%] [Memory Beats: 62.22%] - LeetPush

---
+++ 2011-Final-Value-of-Variable-After-Performing-Operations.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        
+        int x = 0;
+        for (const string& s :operations){
+            if (s[1] == '+'){
+                x++;
+            }
+            else{
+                x--;
+            }
+        }
+        return x;
+    }
+};
