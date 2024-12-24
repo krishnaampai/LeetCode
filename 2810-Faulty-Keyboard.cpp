7d76c41455e886f27a3bb69d4a59204d08eddd60
krishnaampai <147622221+krishnaampai@users.noreply.github.com>
2024-12-24 15:03:26 +0530

    [2810] [Time Beats: 32.72%] [Memory Beats: 67.63%] - LeetPush

---
+++ 2810-Faulty-Keyboard.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string finalString(string s) {
+        string result;
+        for (int i =0;i< s.length();i++){
+            if (s[i] == 'i'){
+               reverse(result.begin(), result.end() );
+             
+            }
+            else {
+               
+                result.push_back(s[i]);
+            }
+            
+        }
+        return result;
+    }
+};
