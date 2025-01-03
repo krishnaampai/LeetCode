4e6a93fd583591fd688ec0b6074ca2f8c0517f16
krishnaampai <147622221+krishnaampai@users.noreply.github.com>
2025-01-03 19:31:27 +0530

    [242] [Time Beats: 12.42%] [Memory Beats: 76.91%] - LeetPush

---
+++ 242-Valid-Anagram.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length()!=t.length()){
+            return false;
+        }
+        unordered_map<char,int> schars,tchars;
+        for (int i=0;i<s.length();i++){
+            s[s[i]]= 1+ 
+        }
+    }
+};
